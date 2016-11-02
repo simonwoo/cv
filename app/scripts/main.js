b0d18cca5cfafade5df56d5b5c8ab12abe3c5be9
WU Chong <simonwoo1991@gmail.com>
2016-11-02 19:51:11 +0100

    feat(cv): wuchong's new cv

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    console.log('成为全宇宙最屌的前端工程师!!!');
+
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: "right"
+    });
+});
---
+++ docs/scripts/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){console.log("成为全宇宙最屌的前端工程师!!!"),$("#fullpage").fullpage({navigation:!0,navigationPosition:"right"})});
