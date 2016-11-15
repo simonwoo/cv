6d1e8e54b24a80d0291a118e08ded0f7a8cf253e
WU Chong <simonwoo1991@gmail.com>
2016-11-16 00:22:52 +0100

    add pdf cv

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var $ = require('gulp-load-plugins')();
 gulp.task('default', ['serve']);
 
 // 静态服务器 + 监听 scss/html 文件
-gulp.task('serve', ['wiredep', 'sass'], function () {
+gulp.task('serve', ['wiredep', 'sass'], function() {
 
     browserSync.init({
         server: "./app"
@@ -26,14 +26,16 @@ gulp.task('serve', ['wiredep', 'sass'], function () {
 gulp.task('clean', del.bind(null, ['docs']));
 
 // scss编译后的css将注入到浏览器里实现更新
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     return gulp.src("app/styles/*.scss")
         .pipe($.sass().on('error', $.sass.logError))
         .pipe(gulp.dest("app/styles"))
-        .pipe(reload({stream: true}));
+        .pipe(reload({
+            stream: true
+        }));
 });
 
-gulp.task('wiredep', function () {
+gulp.task('wiredep', function() {
     gulp.src('app/index.html')
         .pipe(wiredep({
             ignorePath: /^(\.\.\/)*\.\./
@@ -41,16 +43,15 @@ gulp.task('wiredep', function () {
         .pipe(gulp.dest('app'));
 });
 
-gulp.task('styles', ['sass'], function () {
+gulp.task('styles', ['sass'], function() {
     return gulp.src('app/styles/*.css')
-        .pipe($.autoprefixer(
-            {
-                browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
-            }))
+        .pipe($.autoprefixer({
+            browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+        }))
         .pipe(gulp.dest('app/styles'));
 });
 
-gulp.task('html', ['styles'], function () {
+gulp.task('html', ['styles'], function() {
     return gulp.src('app/*.html')
         .pipe($.useref())
         //挑出其中的js文件
@@ -58,11 +59,13 @@ gulp.task('html', ['styles'], function () {
         /* 挑选出其中的css文件 */
         .pipe($.if('*.css', $.minifyCss()))
         .pipe($.useref())
-        .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+        .pipe($.if('*.html', $.htmlmin({
+            collapseWhitespace: true
+        })))
         .pipe(gulp.dest('docs'));
 });
 
-gulp.task('images', function () {
+gulp.task('images', function() {
     return gulp.src('app/images/**/*')
         .pipe($.cache($.imagemin({
             progressive: true,
@@ -71,11 +74,19 @@ gulp.task('images', function () {
         .pipe(gulp.dest('docs/images'));
 });
 
-gulp.task('fonts', function () {
+gulp.task('pdf', function() {
+    return gulp.src('app/pdf/**/*')
+        .pipe(gulp.dest('docs/pdf'));
+});
+
+gulp.task('fonts', function() {
     return gulp.src('app/fonts/**/*')
         .pipe(gulp.dest('docs/fonts'));
 });
 
-gulp.task('build', ['clean', 'html', 'images', 'fonts'], function () {
-    return gulp.src('docs/**/*').pipe($.size({title: 'build', gzip: true}));
+gulp.task('build', ['clean', 'html', 'images', 'pdf', 'fonts'], function() {
+    return gulp.src('docs/**/*').pipe($.size({
+        title: 'build',
+        gzip: true
+    }));
 });
